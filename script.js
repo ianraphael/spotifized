45e9c6e52efd01916ddc605bb9f464a55256b4e0
Ian Labicani <labicaniian@gmail.com>
2022-04-02 12:06:54 +0800

    initial

---
+++ script.js
@@ -0,0 +1,125 @@
+const songs = [
+	{
+		id: 1,
+		img: "song-thumbnails/toxic.jfif",
+		title: "Toxic",
+		author: "BoyWithUke",
+		source: "songs/toxic.mp3",
+		Duration: "2:48",
+		lyric: "All my friends are toxic, all ambitionless So rude and always negative I need new friends, but it's not that quick and easy Oh, I'm drowning, let me breathe I'm better off all by myself Though I'm feelin' kinda empty without somebody else Oh, I hear you cryin' out for help But you never showed for me when I was ringin' your cellphone Oh, you don't know how it feels to be alone Baby, oh, I'll make you know, I'll make you know, oh I'm drowning, let me breathe I'm drowning, let me breathe I'm drowning, let me breathe I'm drowning, let me breathe But life is immaculate, backin' it up a bit Countin' my hours and knocking on wood Avoiding my opposites, chewin' on chocolate Had a bit limited time, but I should Be good for a minute, don't want to admit it I'm running on seconds, I'm rigid, I'm screwed Don't know what to do, I'm thinking of you I'm drinking up bottles and bottles of booze I'm better off all by myself Though I'm feelin' kinda empty without somebody else Oh, I hear you cryin' out for help But you never showed for me when I was ringin' your cellphone Oh, you don't know how it feels to be alone Baby, oh, I'll make you know, I'll make you know, oh I fell into your river That's where you told me lies You said that I'd feel better But this is where good guys die You took my pride away, but You cannot take my life I'll find another way I'll wonder if you're takin' my life Don't you see how I... I'm better off all by myself Though I'm feelin' kinda empty without somebody else Oh, I hear you cryin' out for help But you never showed for me when I was ringin' your cellphone Oh, you don't know how it feels to be alone Baby, oh, I'll make you know, I'll make you know, oh I'm better off all by myself Though I'm feelin' kinda empty without somebody else Oh, I hear you cryin' out for help But you never showed for me when I was ringin' your cellphone Oh, you don't know how it feels to be alone Baby, oh, I'll make you know, I'll make you know, oh"
+	},
+	{
+		id: 2,
+		img: "song-thumbnails/solo.jfif",
+		title: "Solo",
+		author: "Iyaz",
+		source: "songs/solo.mp3",
+		Duration: "3:14",
+		lyric: "Well ya dun know its ya boy I-Y-A-Z Alongside the man J-R'a I said I don't want to walk this earth If I gotta do it, solo. See how we used to be a team Runnin' the streets (yeah) We was living out our dream (oh) You used to be my rider I was your provider Now we separated in two Oh baby you left and sailed away alone (Yeah, alone) And now you got me trapped up on this island With no way to get home And I don't wanna go go I don't wanna go go I don't wanna it no no I don't wanna it no no I don't want to walk this earth If I gotta do it solo (solo) Cause I was so high And now I'm so low And I don't wanna walk around alone, solo Said I don't want to walk this earth If I gotta do it solo (solo) solo You was the beat on to my top line, Put us together And you'd have to hit the rewind See, you give me a purpose Now I'm getting nervous That my heart will never sing again Oh, when we was runnin' up the airways, They knew us from the Virgin Islands to the U.K. See, we was on the way to the platinum and gold Never thought that you'd go but you did ya, ya you did. Oh baby you left and sailed away alone (Yeah, alone) And now you got me trapped up on this island With no way to get home And I don't wanna go go I don't wanna go go I don't wanna it no no I don't wanna it no no I don't want to walk this earth If I gotta do it solo (solo) Cause I was so high And now I'm so low And I don't wanna walk around alone, solo Said I don't want to walk this earth If I gotta do it solo (solo) solo I don't wanna walk S-O-L-O (no no no) I don't wanna walk S-O-L-O Oh no oh, no uh no, Oh no oh no oh no oh I don't wanna walk (S-O-L-O) And I don't wanna go, go I don't wanna go, go I don't wanna it no, no I don't wanna it no, no I don't want to walk this earth If I gotta do it solo (solo) Cause I was so high And now I'm so low And I don't wanna walk around alone (Solo) I said I don't want to walk this earth If I gotta do it solo (solo) solo"
+	},
+	{
+		id: 3,
+		img: "song-thumbnails/deathBed.jfif",
+		title: "Death bed",
+		author: "Powfu, beabadoobee",
+		source: "songs/deathbed.mp3",
+		Duration: "2:53",
+		lyric: "Don't stay awake for too long, don't go to bed I'll make a cup of coffee for your head It'll get you up and going out of bed Yeah, I don't wanna fall asleep, I don't wanna pass away I been thinking of our future, 'cause I'll never see those days I don't know why this has happened, but I probably deserve it I tried to do my best, but you know that I'm not perfect I been praying for forgiveness, you've been praying for my health When I leave this Earth, hoping you'll find someone else 'Cause, yeah, we still young, there's so much we haven't done Getting married, start a family, watch your husband with his son I wish it could be me, but I won't make it out this bed I hope I go to Heaven, so I see you once again My life was kinda short, but I got so many blessings Happy you were mine, it sucks that it's all ending Don't stay awake for too long, don't go to bed I'll make a cup of coffee for your head It'll get you up and going out of bed (yeah, ayy, ayy) Don't stay awake for too long, don't go to bed I'll make a cup of coffee for your head It'll get you up and going out of bed (ayy, yeah) I'm happy that you here with me, I'm sorry if I tear up When me and you were younger, you would always make me cheer up Taking goofy videos and walking through the park You would jump into my arms every time you heard a bark Cuddle in your sheets, sing me sound asleep And sneak out through your kitchen at exactly 1:03 Sundays, went to church, on Mondays, watched a movie Soon you'll be alone, sorry that you have to lose me Don't stay awake for too long, don't go to bed I'll make a cup of coffee for your head It'll get you up and going out of bed Don't stay awake for too long, don't go to bed I'll make a cup of coffee for your head It'll get you up and going out of bed Don't stay awake for too long, don't go to bed I'll make a cup of coffee for your head It'll get you up and going out of bed Don't stay awake for too long, don't go to bed I'll make a cup of coffee for your head It'll get you up and going out of bed Don't stay awake for too long, don't go to bed I'll make a cup of coffee for your head It'll get you up and going out of bed"
+	},
+	{
+		id: 4,
+		img: "song-thumbnails/paumaga.jfif",
+		title: "Paumaga",
+		author: "Je",
+		source: "songs/paumaga.mp3",
+		Duration: "4:25",
+		lyric: "Paumaga nanaman wag na tayong umuwi Anong pwedeng dahilan sa ating paglilibang? Palubog na ang buwan Palitaw na rin ang araw Kwento natin ay nagsisimula pa lang Wag tayo mag alangan sa mga nakaraan Tuwid lagi ang daan wag paikot ikot lang Atin laging pagmasdan ang mga nakaabang Pag di na napuntahan lagi yang may paraan Tara na bitawan yang hawak na alak Halatang tinamaan ka na sorbesa Mga matang papikit pikit lakad mo di na tuwid Akong bahala sa iyo Mahal di kita pababayaan Itong gabi ay para sa atin Gawin mo lang ang gusto mong gawin Tulad ng mga bagay na di pa nagawa Tayo'y mag saya… mag saya Mag saya… mag saya Paumaga nanaman wag na tayong umuwi Anong pwedeng dahilan sa ating paglilibang? Palubog na ang buwan Palitaw na rin ang araw Kwento natin ay nagsisimula pa lang Wag tayo mag alangan sa mga nakaraan Tuwid lagi ang daan wag paikot ikot lang Atin laging pagmasdan ang mga nakaabang Pag di na napuntahan lagi yang may paraan May oras pa sa ating bulsa 'Di bale ng walang lamang pera basta may oras pa Heto nanaman hindi ako mapakali Gusto na makasama ka umaga hanggang sa gabi Gusto kitang makatabi Sa mga bawat na sandali Sa dalampasigan, ikaw ay aking aawitan Habang pinagmamasdan ko lang ang iyong kagandahan Napakasarap mo kasing titigan Ang mga problema kalungkutan ay nalilimutan Sindihan natin yung bonfire ilagay sa may tabe Pag nawala sa paningin hindi ako mapakali Mag uumaga nanaman o mamaya na umuwi Gusto kita na makasama pa mayakap pang muli Paumaga nanaman wag na tayong umuwi Anong pwedeng dahilan sa ating paglilibang? Palubog na ang buwan Palitaw na rin ang araw Kwento natin ay nagsisimula pa lang Wag tayo mag alangan sa mga nakaraan Tuwid lagi ang daan wag paikot ikot lang Atin laging pagmasdan ang mga nakaabang Pag di na napuntahan lagi yang may paraan May oras pa sa ating bulsa 'Di bale ng walang lamang pera basta may oras pa May oras pa, may oras pa May oras pa, may oras pa May oras pa, may oras pa May oras pa, may oras pa May oras pa, may oras pa l, aye! Gusto pa kitang makasamac, aye! Wag na tayong umuwi Sayo na 'ko pauwi, aye! May oras pa, may oras pa l, aye! Gusto pa kitang makasamac, aye! Wag na tayong umuwi Sayo nako pauwi, aye!"
+	},
+	{
+		id: 5,
+		img: "song-thumbnails/comethru.jfif",
+		title: "comethru",
+		author: "Jeremy Zucker",
+		source: "songs/comethru.mp3",
+		Duration: "3:01",
+		lyric: "I might lose my mind Waking when the sun's down Riding all these highs Waiting for the comedown Walk these streets with me I'm doing decently Just glad that I can breathe, yeah I'm trying to realize It's alright to not be fine on your own Now I'm shaking, drinking all this coffee These last few weeks have been exhausting I'm lost in my imagination And there's one thing that I need from you Can you come through, through? Through, yeah And there's one thing that I need from you Can you come through? Ain't got much to do Too old for my hometown Went to bed at noon Couldn't put my phone down Scrolling patiently It's all the same to me Just faces on a screen, yeah I'm trying to realize It's alright to not be fine on your own Now I'm shaking, drinking all this coffee These last few weeks have been exhausting I'm lost in my imagination And there's one thing that I need from you Can you come through, through? Through, yeah And there's one thing that I need from you Can you come Through, through Through, yeah And there's one thing that I need from you Can you come through?"
+	}
+]
+const audioTag = document.querySelector("#audioTag");
+const title = document.querySelector("#title");
+const singer = document.querySelector("#singer");
+const lyric = document.querySelector("#lyric");
+const thumbnail = document.querySelector("#thumbnail");
+const songNumber = document.querySelector("#song-number");
+const prevBtn = document.querySelector("#prev");
+const playPauseBtn = document.querySelector("#playIcon");
+const nextBtn = document.querySelector("#next");
+
+songNumber.textContent = songs.length;
+// display songs
+songs.forEach((song, i) => {
+	document.querySelector("tbody").innerHTML += `
+	<tr id="${i + 1}">
+		<td>${song.id}</td>
+		<td><img src="${song.img}" alt="" width="50px"></td>
+		<td>${song.title}</td>
+		<td>${song.author}</td>
+		<td>${song.Duration}</td>
+	</tr>`;
+});
+document.querySelector("tbody").addEventListener("click", (e) => {
+	playAudio(e.target.parentElement.id)
+	for(let a = 0; a < document.querySelectorAll("tr").length; a++) {
+		document.querySelectorAll("tr")[a].classList.remove("active");
+	}
+	e.target.parentElement.classList.add("active");
+	play = true;
+	document.querySelector("#playIcon").classList.remove("fa-play")
+	document.querySelector("#playIcon").classList.add("fa-pause")
+
+})
+let currentId = 0;
+const playAudio = (songId) => {
+	audioTag.setAttribute("src", `${songs[songId-1].source}`);
+	audioTag.load();
+	audioTag.play();
+	title.textContent = songs[songId-1].title;
+	singer.textContent = songs[songId-1].author;
+	lyric.textContent = songs[songId-1].lyric;
+	thumbnail.setAttribute("src", `${songs[songId-1].img}`);
+	currentId = songId-1;
+}
+let play = false;
+playPauseBtn.addEventListener("click", () => {
+	document.querySelector("#playIcon").classList.toggle("fa-pause");
+	document.querySelector("#playIcon").classList.toggle("fa-play");
+	if (play === false) {
+	audioTag.play();
+	play = true;
+	}else {
+		audioTag.pause();
+		play = false;
+	} 
+})
+
+prevBtn.addEventListener("click", () => {
+	currentId--;
+	audioTag.setAttribute("src", `${songs[currentId].source}`);
+	audioTag.load();
+	audioTag.play();
+	title.textContent = songs[currentId].title;
+	singer.textContent = songs[currentId].author;
+	lyric.textContent = songs[currentId].lyric;
+	thumbnail.setAttribute("src", `${songs[currentId].img}`);
+})
+
+nextBtn.addEventListener("click", () => {
+	currentId++;
+	audioTag.setAttribute("src", `${songs[currentId].source}`);
+	audioTag.load();
+	audioTag.play();
+	title.textContent = songs[currentId].title;
+	singer.textContent = songs[currentId].author;
+	lyric.textContent = songs[currentId].lyric;
+	thumbnail.setAttribute("src", `${songs[currentId].img}`);
+})
